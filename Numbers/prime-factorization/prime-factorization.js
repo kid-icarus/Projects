83f4725ee9fddcf56869a7882763c73d7df0c1ca
Ryan Kois <ryan.kois@gmail.com>
2013-07-14 10:51:09 -0600

    First commit, prime factorization.

---
+++ Numbers/prime-factorization/prime-factorization.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){  
+  $('#submit').click(function(event){
+    event.preventDefault();
+    var num = $('#number').val();
+    if ($.isNumeric(num)) {
+      factor(num); 
+    }
+  });
+});
+
+function factor(num) {
+  var i;
+  var factors = [];
+  for(i = 2; i < num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+  console.log(factors);
+  print_factors(factors);
+}
+
+function print_factors(factors) {
+  var i,
+      answer = '';
+  for (i = 0; i < factors.length; i++) {
+    answer += factors[i] + ", ";
+  }
+  $('#answer').html(answer);
+}
