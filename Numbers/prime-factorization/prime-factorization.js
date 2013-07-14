4bd7044fff38df639d01d046ddf853ac475eca3b
Ryan Kois <ryan.kois@gmail.com>
2013-07-14 11:42:41 -0600

    Get rid of the last comma.

--- Numbers/prime-factorization/prime-factorization.js
+++ Numbers/prime-factorization/prime-factorization.js
@@ -24,7 +24,12 @@ function print_factors(factors) {
   var i,
       answer = '';
   for (i = 0; i < factors.length; i++) {
-    answer += factors[i] + ", ";
+    if (i === factors.length - 1) {
+      answer += factors[i];
+    }
+    else {
+      answer += factors[i] + ", ";
+    }
   }
   $('#answer').html(answer);
 }
